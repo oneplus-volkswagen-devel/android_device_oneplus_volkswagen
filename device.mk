5455ffd56e0ac3d1c895782f09c239d2f533287c
grepfox <grepfox@tutamail.com>
2025-11-08 23:40:13 +0530

    volkswagen: Make Action Button an assistant key
    gpio-keys.kl keylayouts kanged from oxygenos with assist key mapped
    
    Change-Id: Ida9c1a5c2d5613002cd2e9ea46d7434472046f11
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- device.mk
+++ device.mk
@@ -8,6 +8,10 @@
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
+# Action button
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
