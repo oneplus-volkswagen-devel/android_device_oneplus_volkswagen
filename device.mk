9412ae0fb45a6e195687bac4565718b0c816a3ce
SphericalKat <amolele@gmail.com>
2026-09-13 12:41:22 +0200

    volkswagen: Setup regional system props
    Load model and region properties dynamically for each OnePlus Nord 6 regional variant.
    
    Change-Id: Idba714751b8b62f12f62d9b365e1c679b448f8cf
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- device.mk
+++ device.mk
@@ -50,11 +50,10 @@ PRODUCT_PACKAGES += \
 
 # Regional properties
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/vendor/odm/etc/23821/build.default.prop:$(TARGET_COPY_OUT_ODM)/etc/23821/build.default.prop \
-    $(LOCAL_PATH)/recovery/root/vendor/odm/etc/23893/build.EU.prop:$(TARGET_COPY_OUT_ODM)/etc/23893/build.EU.prop \
-    $(LOCAL_PATH)/recovery/root/vendor/odm/etc/23893/build.IN.prop:$(TARGET_COPY_OUT_ODM)/etc/23893/build.IN.prop \
-    $(LOCAL_PATH)/recovery/root/vendor/odm/etc/23893/build.NA.prop:$(TARGET_COPY_OUT_ODM)/etc/23893/build.NA.prop \
-    $(LOCAL_PATH)/recovery/root/vendor/odm/etc/23893/build.default.prop:$(TARGET_COPY_OUT_ODM)/etc/23893/build.default.prop
+    $(LOCAL_PATH)/recovery/root/vendor/odm/etc/24885/build.default.prop:$(TARGET_COPY_OUT_ODM)/etc/24885/build.default.prop \
+    $(LOCAL_PATH)/recovery/root/vendor/odm/etc/24885/build.EU.prop:$(TARGET_COPY_OUT_ODM)/etc/24885/build.EU.prop \
+    $(LOCAL_PATH)/recovery/root/vendor/odm/etc/24885/build.IN.prop:$(TARGET_COPY_OUT_ODM)/etc/24885/build.IN.prop \
+    $(LOCAL_PATH)/recovery/root/vendor/odm/etc/24885/build.ROW.prop:$(TARGET_COPY_OUT_ODM)/etc/24885/build.ROW.prop
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
