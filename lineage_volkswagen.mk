21143552d8457113e1d6b65b79c47be09a8ef07b
Omkar Chandorkar <gotenksIN@aospa.co>
2026-09-14 17:35:56 +0530

    volkswagen: Enable insecure ADB for early bringup
    Set WITH_ADB_INSECURE := true to disable USB debugging authentication
    and ensure adbd starts as root on both Android OS and recovery.
    
    Change-Id: I298b47e27944f2c06927bf31792d47cf7eb8489e
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- lineage_volkswagen.mk
+++ lineage_volkswagen.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from volkswagen device
 $(call inherit-product, device/oneplus/volkswagen/device.mk)
 
+# Insecure ADB
+WITH_ADB_INSECURE := true
+
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
