24ad5d1cace8f31dfa78417e4349684ba65296b7
SphericalKat <amolele@gmail.com>
2026-09-15 05:23:10 +0200

    Revert "dodge: Add KeyHandler and configs"
    This reverts commit b0f26f7e5c9aea73a4c3f0b0279296fcdca084e5.

--- device.mk
+++ device.mk
@@ -8,9 +8,6 @@
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
-# Alert slider
-PRODUCT_PACKAGES +=     KeyHandler     tri-state-key-calibrate
-
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
